92ec630562252a8b2a465ecf801e90d42fb75202
Luiz Carlos Araujo <37367902+LuizC-Araujo@users.noreply.github.com>
2022-11-10 22:13:17 -0300

    adicionado context, models, mapping e migrations

---
+++ Models/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Blog.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
