7c9acc30b48d3270fa9c42b70339e37db60f1ad4
Luiz Carlos Araujo <37367902+LuizC-Araujo@users.noreply.github.com>
2022-11-12 22:47:01 -0300

    tratando erros categoria

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -12,8 +12,15 @@ public class CategoryController : ControllerBase
     [HttpGet("")]
     public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context)
     {
-        var categories = await context.Categories.ToListAsync();
-        return Ok(categories);
+        try
+        {
+            var categories = await context.Categories.ToListAsync();
+            return Ok(categories);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, "CATX01 - Falha interna no servidor");
+        }
     }
     
     [HttpGet("{id:int}")]
@@ -21,14 +28,21 @@ public class CategoryController : ControllerBase
         [FromRoute] int id,
         [FromServices] BlogDataContext context)
     {
-        var category = await context
-            .Categories
-            .FirstOrDefaultAsync();
+        try
+        {
+            var category = await context
+                .Categories
+                .FirstOrDefaultAsync();
 
-        if (category == null)
-            return NotFound();
+            if (category == null)
+                return NotFound();
         
-        return Ok(category);
+            return Ok(category);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, "CATX02 - Falha interna no servidor");
+        }
     }
     
     [HttpPost("")]
@@ -45,51 +59,73 @@ public class CategoryController : ControllerBase
         }
         catch (DbUpdateException ex)
         {
-            return StatusCode(500, "Não foi possível incluir a categoria");
+            return StatusCode(500, "CATX03 - Não foi possível incluir a categoria");
         }
         catch (Exception e)
         {
-            return StatusCode(500, "Falha interna no servidor");
+            return StatusCode(500, "CATX04 - Falha interna no servidor");
         }
     }
-    
+
     [HttpPut("{id:int}")]
     public async Task<IActionResult> PutAsync(
         [FromRoute] int id,
         [FromBody] Category model,
         [FromServices] BlogDataContext context)
     {
-        var category = await context
-            .Categories
-            .FirstOrDefaultAsync(x => x.Id == id);
-        
-        if (category == null)
-            return NotFound();
+        try
+        {
+            var category = await context
+                .Categories
+                .FirstOrDefaultAsync(x => x.Id == id);
 
-        category.Name = model.Name;
-        category.Slug = model.Slug;
+            if (category == null)
+                return NotFound();
 
-        context.Categories.Update(category);
-        await context.SaveChangesAsync();
+            category.Name = model.Name;
+            category.Slug = model.Slug;
 
-        return Created($"v1/categories/{model.Id}", model);
+            context.Categories.Update(category);
+            await context.SaveChangesAsync();
+
+            return Created($"v1/categories/{model.Id}", model);
+        }
+        catch (DbUpdateException)
+        {
+            return StatusCode(500, "CATX05 - Não foi possível atualizar a categoria");
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, "CATX06 - Falha interna no servidor");
+        }
     }
-    
+
     [HttpDelete("{id:int}")]
     public async Task<IActionResult> DeleteAsync(
         [FromRoute] int id,
         [FromServices] BlogDataContext context)
     {
-        var category = await context
-            .Categories
-            .FirstOrDefaultAsync(x => x.Id == id);
-        
-        if (category == null)
-            return NotFound();
+        try
+        {
+            var category = await context
+                .Categories
+                .FirstOrDefaultAsync(x => x.Id == id);
 
-        context.Categories.Remove(category);
-        await context.SaveChangesAsync();
+            if (category == null)
+                return NotFound();
 
-        return Ok(category);
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
+            
+            return Ok(category);
+        }
+        catch (DbUpdateException)
+        {
+            return StatusCode(500, "CATX07 - Não foi possível deletar a categoria");
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, "CATX08 - Falha interna no servidor");
+        }
     }
 }
