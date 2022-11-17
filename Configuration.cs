d0c6a9218fb346658d8d9f1d05e230414a184435
Luiz Carlos Araujo <37367902+LuizC-Araujo@users.noreply.github.com>
2022-11-17 15:24:21 -0300

    lendo as configurações do appsettings

--- Configuration.cs
+++ Configuration.cs
@@ -5,4 +5,13 @@ public static class Configuration
     public static string JwtKey = "FC5EVHALfEqIRah1XMbhUw==";
     public static string ApiKeyName = "api_key";
     public static string ApiKey = "tDYO81qj6EC29aMYdzOiOQ==";
+    public static SmtpConfiguration Smtp = new();
+
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; } = 25;
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
 }
