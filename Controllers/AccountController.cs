a38553729e14103d12552c903678b2d5bdb2e073
Luiz Carlos Araujo <37367902+LuizC-Araujo@users.noreply.github.com>
2022-11-17 19:15:44 -0300

    implementando upload de imagem para a api

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,8 +1,12 @@
-﻿using Blog.Data;
+﻿using System.Text.RegularExpressions;
+using Blog.Data;
 using Blog.Extensions;
+using Blog.Models;
 using Blog.Services;
 using Blog.ViewModel.Accounts;
 using Blog.ViewModel.Results;
+using Blog.ViewModel.UploadImage;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SecureIdentity.Password;
@@ -62,6 +66,47 @@ public class AccountController : ControllerBase
         }
     }
 
+    [Authorize]
+    [HttpPost("upload-image")]
+    public async Task<IActionResult> UploadImage(
+        [FromBody] UploadImageViewModel model,
+        [FromServices] BlogDataContext context)
+    {
+        var fileName = $"{Guid.NewGuid().ToString()}.jpg";
+        var data = new Regex(@"^data:image\/[a-z]+;base64,")
+            .Replace(model.Base64Image, "");
+        var bytes = Convert.FromBase64String(data);
+
+        try
+        {
+            await System.IO.File.WriteAllBytesAsync($"wwwroot/images/{fileName}", bytes);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, new ResultViewModel<string>("ACCX02 - Falha interna no servidor"));
+        }
+
+        var user = await context.Users
+            .FirstOrDefaultAsync(x => x.Email == User.Identity.Name);
+
+        if (user is null)
+            return NotFound(new ResultViewModel<User>("ACCX03 - Usuário não encontrado"));
+
+        user.Image = $"https://localhost:7152/images/{fileName}";
+
+        try
+        {
+            context.Users.Update(user);
+            await context.SaveChangesAsync();
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<string>("ACCX04 - Falha Interna no servidor"));
+        }
+
+        return Ok(new ResultViewModel<string>("Imagem alterada com sucesso!", null));
+    }
+    
     // [Authorize(Roles = "user")]
     // [HttpGet("user")]
     // public IActionResult GetUser() => Ok(User.Identity.Name);
