231701cf1dccb6cbf0dde9dad2c3c6253e5ffbae
Luiz Carlos Araujo <37367902+LuizC-Araujo@users.noreply.github.com>
2022-11-16 23:02:58 -0300

    implementando ApiKey

--- Configuration.cs
+++ Configuration.cs
@@ -2,5 +2,7 @@
 
 public static class Configuration
 {
-    public static string JwtKey { get; set; } = "FC5EVHALfEqIRah1XMbhUw==";
+    public static string JwtKey = "FC5EVHALfEqIRah1XMbhUw==";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "tDYO81qj6EC29aMYdzOiOQ==";
 }
