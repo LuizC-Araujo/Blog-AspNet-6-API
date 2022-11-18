29562b1016ce920fff01d01237c50db6b75e61cc
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-11-18 18:54:53 -0300

    usando cache

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using Blog.ViewModel.Categories;
 using Blog.ViewModel.Results;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Blog.Controllers;
 
@@ -13,11 +14,17 @@ namespace Blog.Controllers;
 public class CategoryController : ControllerBase
 {
     [HttpGet("")]
-    public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context)
+    public async Task<IActionResult> GetAsync(
+        [FromServices] IMemoryCache cache,
+        [FromServices] BlogDataContext context)
     {
         try
         {
-            var categories = await context.Categories.ToListAsync();
+            var categories = cache.GetOrCreate("CategoriesCache", entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1);
+                return GetCategories(context);
+            });
             return Ok(new ResultViewModel<List<Category>>(categories));
         }
         catch
@@ -25,7 +32,12 @@ public class CategoryController : ControllerBase
             return StatusCode(500, new ResultViewModel<List<Category>>("CATX01 - Falha interna no servidor"));
         }
     }
-    
+
+    private List<Category> GetCategories(BlogDataContext context)
+    {
+        return context.Categories.ToList();
+    }
+
     [HttpGet("{id:int}")]
     public async Task<IActionResult> GetByIdAsync(
         [FromRoute] int id,
